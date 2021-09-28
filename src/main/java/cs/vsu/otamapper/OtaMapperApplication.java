c139f078b036e6e6f74147a96bbb637c005f5cb7
dmikhalev <mikhalev.dmitrii.2000@mail.ru>
2021-09-28 12:04:38 +0300

    Create project

---
+++ src/main/java/cs/vsu/otamapper/OtaMapperApplication.java
@@ -0,0 +1,13 @@
+package cs.vsu.otamapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtaMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtaMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cs/vsu/otamapper/OtaMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.vsu.otamapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtaMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
